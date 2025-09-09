a5b470ea021fb6b934b65a03cbe9084ad4bd1486
Marabooo <354536@viauc.dk>
2025-09-09 12:09:26 +0200

    Basic Comment User Post

--- Server/Entities/Comment.cs
+++ Server/Entities/Comment.cs
@@ -1,9 +1,9 @@
 ﻿namespace Entities;
 
-public class Post
+public class Comment
 {
     public int Id { get; set; }
-    public string Title { get; set; }
     public string Body { get; set; }
     public int UserId { get; set; }
+    public int PostId {get; set;}
 }
