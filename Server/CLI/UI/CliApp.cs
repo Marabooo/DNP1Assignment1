da3c72cbf33336976aadf5e9d7f83d513f055ce8
Marabooo <354536@viauc.dk>
2025-09-16 10:15:32 +0200

    Create basic views

---
+++ Server/CLI/UI/CliApp.cs
@@ -0,0 +1,14 @@
+﻿using Entities;
+using RepositoryContracts;
+
+namespace CLI.UI;
+
+public class CliApp (IUserRepository userRepository, ICommentRepository commentRepository, IPostRepository postRepository)
+{
+    public async Task<Post> StartAsync()
+    {
+        return null;
+    }
+    
+
+}
