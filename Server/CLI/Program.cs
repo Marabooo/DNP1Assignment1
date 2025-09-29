f5038e8bd3432273dc8dd534cd4ef7237782c775
Marabooo <354536@viauc.dk>
2025-09-29 10:27:25 +0200

    Fixed References

--- Server/CLI/Program.cs
+++ Server/CLI/Program.cs
@@ -3,12 +3,12 @@
 using System.Runtime.InteropServices.ComTypes;
 using CLI.UI;
 using RepositoryContracts;
-//using FileRepositories;
+using FileRepositories;
 
 Console.WriteLine("Starting CLI app...");
-//IUserRepository userRepository = new UserFileRepository();
-//IPostRepository postRepository = new PostFileRepository();
-//ICommentRepository commentRepository = new CommentFileRepository();
+IUserRepository userRepository = new UserFileRepository();
+IPostRepository postRepository = new PostFileRepository();
+ICommentRepository commentRepository = new CommentFileRepository();
 
-//CliApp cliApp = new CliApp(userRepository, commentRepository, postRepository);
-//await cliApp.StartAsync();
+CliApp cliApp = new CliApp(userRepository, commentRepository, postRepository);
+await cliApp.StartAsync();
