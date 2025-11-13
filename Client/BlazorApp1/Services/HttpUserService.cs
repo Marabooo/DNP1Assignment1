fd86771e9ef2f031a3f6629dba3eeed7ddf8dc59
Marabooo <354536@viauc.dk>
2025-11-13 21:42:27 +0100

    small commit. created blazor app

---
+++ Client/BlazorApp1/Services/HttpUserService.cs
@@ -0,0 +1,28 @@
+﻿using ApiContracts_DTOs;
+
+namespace BlazorApp1.Services;
+
+public class HttpUserService
+{
+    private readonly HttpClient Client;
+
+    public HttpUserService(HttpClient client)
+    {
+        this.Client = client;
+    }
+
+    public Task<UserDto> AddUserAsync(CreateUserDto request)
+    {
+        //todo
+        return null;
+    }
+
+    public Task UpdateUserAsync(int id, UpdateUserDTO request)
+    {
+        //todo
+        return null;
+    }
+    
+    //more methods
+
+}
---
+++ Client/BlazorApp1/Services/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using ApiContracts_DTOs;
+
+namespace BlazorApp1.Services;
+
+public interface IUserService
+{
+    public Task<UserDto> AddUserAsync(CreateUserDto request);
+    public Task UpdateUserAsync(int id, UpdateUserDTO request);
+}
